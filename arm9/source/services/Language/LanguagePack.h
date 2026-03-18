c96545f7f88fe1f37dbdd959ea373ae5ed944554
mighty_Jun <junhuk1113@naver.com>
2026-03-19 00:27:42 +0900

    Increase buffer size for no cheats message in LanguagePack

--- arm9/source/services/Language/LanguagePack.h
+++ arm9/source/services/Language/LanguagePack.h
@@ -10,5 +10,5 @@ public:
     String<char, 32> displaySettings_sorting = "Sorting";
     String<char, 32> displaySettings_filters = "Filters";
     String<char, 32> cheatSettings_title = "Cheat";
-    String<char, 32> cheatSettings_noCheatsMsg = "No cheats found.";
+    String<char, 64> cheatSettings_noCheatsMsg = "No cheats found.";
 };
